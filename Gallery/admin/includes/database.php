404c689364e69927a857d5cbb96fafcdfdc37079
Rumahu <rumahu@gmail.com>
2017-09-18 14:19:51 -0500

    Implement mysqli object inside of database. More OOP based.

--- Gallery/admin/includes/database.php
+++ Gallery/admin/includes/database.php
@@ -10,28 +10,33 @@ class Database{
 	}
 	
 	public function open_db_connection(){
-		$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+		$this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 		
-		if(mysqli_connect_errno()){
-			die("Database connection failed" . mysqli_error());
+		if($this->connection->connect_errno){
+			die("Database connection failed " . $this->connection->connect_error);
 		}		
 	}
 	
 	public function query($sql){
-		$result = mysqli_query($this->connection, $sql);
+		$result = $this->connection->query($sql);
 		
+		$this->confirmQuery($result);
 		
 		return $result;
 	}
 	
 	private function confirmQuery($result){
 		if(!$result){
-			die("DB Query failed.");
+			die("DB Query failed " . $this->connection->error);
 		}
 	}
 	
 	public function escapeString($string){
-		return mysqli_real_escape_string($this->connection, $string);		
+		return $this->connection->real_escape_string($string);	
+	}
+	
+	public function theInsertId(){
+		return $this->connection->insert_id;
 	}
 
 }
