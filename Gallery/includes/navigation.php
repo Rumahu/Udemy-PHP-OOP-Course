83a319f6d212cee10a42675d724eec4e76fb3572
Rumahu <rumahu@gmail.com>
2017-09-18 12:40:11 -0500

    Refactor index.php PHP to new files.

--- Gallery/includes/navigation.php
+++ Gallery/includes/navigation.php
@@ -20,7 +20,7 @@
                         <a href="#">Services</a>
                     </li>
                     <li>
-                        <a href="#">Contact</a>
+                        <a href="#">Admin</a>
                     </li>
                 </ul>
             </div>
